f26e2f9f11aac5cd36658ade3c8d98051adf5454
Shuo Chang <shuo@outlook.com>
2014-01-26 21:28:23 -0500

    constraints checking methods (compiling)

--- sudoku.go
+++ sudoku.go
@@ -6,40 +6,45 @@ import (
 	"strconv"
 )
 
-// Blank data structure
+// blank data structure
 // Represents a variable that may or may not be filled
-type Blank struct {
+type blank struct {
 	value, row, col int
 }
 
 // Returns a instance of blank
-func makeBlank(value, row, col int) Blank {
-	return Blank{value, row, col}
+func makeBlank(value, row, col int) blank {
+	return blank{value, row, col}
 }
 
 // Returns whether a blank is empty
-func (b Blank) IsEmpty() bool {
+func (b blank) isEmpty() bool {
 	return b.value == 0
 }
 
 // Returns a pretty string value of the blank for printing
-func (b Blank) PrettyValue() string {
-	if b.IsEmpty() {
+func (b blank) prettyValue() string {
+	if b.isEmpty() {
 		return "⬚"
 	}
 	return strconv.Itoa(b.value)
 }
 
+// box data structure
+// Represents a box region in the puzzle.
+// Is an array to pointers of blanks that are in the box
+type box [9]*blank
+
 // Sudoku data structure
 // Represents an abstraction of the Sudoku board
 // Tracks empty blanks
 type Sudoku struct {
-	blanks [9][9]Blank
-	empty  []*Blank
+	blanks [9][9]blank
+	boxes  [3][3]box
 }
 
 // Returns a pointer to a new instance of Sudoku
-func newSudoku(source string) *Sudoku {
+func NewSudoku(source string) *Sudoku {
 	bytes := []byte(source)
 
 	if len(bytes) != 81 {
@@ -58,40 +63,24 @@ func newSudoku(source string) *Sudoku {
 	return sudoku
 }
 
-// Helper function that panics if Blank index is out of bound
+// Helper function that panics if blank index is out of bound
 func assertRange(row, col int) {
 	if row < 0 || col < 0 || row > 8 || col > 8 {
 		panic(fmt.Sprintf("Blank is out of range: row %d, column %d", row, col))
 	}
 }
 
-// Get a pointer to a Blank with index (row, col)
-func (s *Sudoku) Get(row, col int) *Blank {
+// Get a pointer to a blank with index (row, col)
+func (s *Sudoku) Get(row, col int) *blank {
 	assertRange(row, col)
 	return &(s.blanks[row][col])
 }
 
-// Set value of (row, col) to val, updates the empty list
+// Set value of (row, col) to val
 func (s *Sudoku) Set(row, col, val int) {
 	assertRange(row, col)
-	// G
-	b := s.Get(row, col)
-	if val == 0 {
-
-	}
+	s.Get(row, col).value = val
 }
-
-/*
-type function func(Blank *b)
-
-func (s *Sudoku) Map(function) {
-	for row := 0; row < 9; row++ {
-		for col := 0; col < 9; col++ {
-			function(s.Get(row, col))
-		}
-	}
-}
-*/
 
 // Print the beautiful board
 func (s *Sudoku) PrettyPrint() {
@@ -100,7 +89,7 @@ func (s *Sudoku) PrettyPrint() {
 	for row := 0; row < 9; row++ {
 		fmt.Print("|")
 		for col := 0; col < 9; col++ {
-			fmt.Print(s.Get(row, col).PrettyValue())
+			fmt.Print(s.Get(row, col).prettyValue())
 			if (col+1)%3 == 0 {
 				fmt.Print("|")
 			} else {
@@ -115,14 +104,46 @@ func (s *Sudoku) PrettyPrint() {
 	fmt.Print(edgeSep) // bottom horizontal separator
 }
 
+func (s *Sudoku) isRowConsistent(b *blank) bool {
+	for _, newB := range s.blanks[b.row] {
+		if newB.value == b.value && newB.col != b.col {
+			return false
+		}
+	}
+	return true
+}
+func (s *Sudoku) isColConsistent(b *blank) bool {
+	col := b.col
+	for row := 0; row < 9; row++ {
+		newB := s.blanks[row][col]
+		if newB.value == b.value && row != b.row {
+			return false
+		}
+	}
+	return true
+}
+func (s *Sudoku) getBox(b *blank) *box {
+	return &(s.boxes[b.row/3][b.col/3])
+}
+func (s *Sudoku) isBoxConsistent(b *blank) bool {
+	for _, newB := range s.getBox(b) {
+		if b.value == newB.value &&
+			b.row == newB.row &&
+			b.col == newB.col {
+			return false
+		}
+	}
+	return true
+}
+
 // Check if Block b is consistent regarding Sudoku game rules
-func (s *Sudoku) IsConsistent(b *Blank) bool {
+func (s *Sudoku) isConsistent(b *blank) bool {
 	// Unfilled blank is always consistent
-	if b.IsEmpty() {
+	if b.isEmpty() {
 		return true
 	}
 	// Check row, column, and box for dupe
-	if s.IsRowConsistent(b.row) && s.IsColConsistent(b.col) && s.IsBoxConsistent(b.box) {
+	if s.isRowConsistent(b) && s.isColConsistent(b) && s.isBoxConsistent(b) {
 		return true
 	}
 	return false
@@ -132,9 +153,9 @@ func (s *Sudoku) IsConsistent(b *Blank) bool {
 func (s *Sudoku) IsComplete() (bool, error) {
 	for row := 0; row < 9; row++ {
 		for col := 0; col < 9; col++ {
-			if b := s.Get(row, col); b.IsEmpty() { // if b is empty, then it's not complete
+			if b := s.Get(row, col); b.isEmpty() { // if b is empty, then it's not complete
 				return false, nil
-			} else if s.IsConsistent(b) == false { // if b is not consistent, then it's not complete
+			} else if s.isConsistent(b) == false { // if b is not consistent, then it's not complete
 				return false, errors.New(fmt.Sprintf("inconsistent blank found at row %d, column %d", row, col))
 			}
 		}
@@ -142,15 +163,18 @@ func (s *Sudoku) IsComplete() (bool, error) {
 	return true, nil
 }
 
-// Main AI entrance point
+// Solves puzzle by performing backtracking search
 func (s *Sudoku) Solve() error {
+	// check completeness and correctness
 	if complete, err := s.IsComplete(); complete || err != nil {
 		return err
 	}
+	//
+
 	return nil
 }
 
 func main() {
 	source := "030080006500294710000300500005010804420805039108030600003007000041653002200040060"
-	newSudoku(source).PrettyPrint()
+	NewSudoku(source).PrettyPrint()
 }
